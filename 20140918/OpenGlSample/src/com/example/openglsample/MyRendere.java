92acfda455a4575b19163cfa853bb22e16c2200d
iberianpig <yhkyky@gmail.com>
2014-09-18 12:48:43 +0900

    changed

--- 20140918/OpenGlSample/src/com/example/openglsample/MyRendere.java
+++ 20140918/OpenGlSample/src/com/example/openglsample/MyRendere.java
@@ -7,19 +7,35 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
 
 public class MyRendere implements Renderer {
 
+	MyCube cube = new MyCube();
+
 	@Override
-	public void onDrawFrame(GL10 arg0) {
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(
+			GL10.GL_COLOR_BUFFER_BIT|GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0, 0, -3f);
+		cube.draw(gl);
 	}
 
 	@Override
-	public void onSurfaceChanged(GL10 arg0, int arg1, int arg2) {
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glViewport(0, 0, width, height);
+
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		GLU.gluPerspective(gl, 45f, (float)width/height, 1f, 50f);
 	}
 
 	@Override
-	public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL10.GL_LEQUAL);
 	}
 
 }
